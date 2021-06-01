61d2f89b24dfa483b6d6a31d5d118c5d7bab788c
eric <erichoward2141@gmail.com>
2021-06-01 09:52:52 -0500

    Sparal 1.4
    styling layout for Product Screen using react-bootstrap props
    import mock products data to use in product screen component
    map through product data and deconstruct prop in layout to display on screen
    import Ratings component to be used in product screen
    use listgroup to add logic for stock status to display on screen
    add to cart button styling including disable feat if not in stock

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ListGroup, Col, Row } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id)
   return (
-    <div>Product Screen</div>
+    <section className="py-5">
+    <Link className='btn btn-dark my-3' to='/'> Go Back</Link>
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="row gx-4 gx-lg-5 align-items-center">
+                <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={product.image} alt="..." /></div>
+                <div className="col-md-6">
+                    <div className="small mb-1">SKU: BST-498</div>
+                    <h1 className="display-5 fw-bolder">{product.name}</h1>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                    </ListGroup.Item>
+                    <div className="fs-5 mb-5">
+                        <ListGroup.Item>${product.price}</ListGroup.Item>
+                    </div>
+                    <p className="lead">{product.description}</p>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                          {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <div className="d-flex">
+                        <input className="form-control text-center me-3" id="inputQuantity" type="num" value="1" style={ { maxWidth: '3rem' } } />
+                        <button className="btn btn-outline-dark flex-shrink-0" type="button" disabled={product.countInStock === 0}>
+                            <i className="bi-cart-fill me-1"></i>
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </section>
   )
 }
 export default ProductScreen
