9cc55230c50ee86569469e0eb2debc0d4641d6f0
eric <erichoward2141@gmail.com>
2021-06-05 18:03:26 -0500

    Sparal v1.5.4
    create a routes folder to seperate routes from server code
    import express and set router variable
    cut/paste router code from server into the product routes file
    remove the product data mock file and import the product routes into the server file
    import Product model into the route file and set product to the product model
    download async middleware and wrap it to the async code in the router
    change the product data to find by id in the show single product
    test routes in postman, error was returning html
    create a custom middleware for error and wanted to return the stack
    created another error handler that handles 404 errors that return the original url if not found

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDb from './config/db.js'
-import products from './data/products.js'
+// custom routes
+import productRoutes from './routes/product_routes.js'
+// custom middleware
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -14,15 +17,14 @@ app.get('/', (req, res ) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res ) => {
-    res.json(products)
-  })
+app.use('/api/products', productRoutes)
 
-  app.get('/api/products/:id', (req, res ) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-  })
+// error handler for 404
+app.use(notFound)
 
-  const PORT = process.env.PORT || 5000
+// custom middleware for error
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)) 
