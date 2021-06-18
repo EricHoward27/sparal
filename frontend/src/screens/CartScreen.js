937a21e288f1b971024cd946618fbe0dca299adf
eric <erichoward2141@gmail.com>
2021-06-18 13:01:14 -0500

    Sparal v1.6.3
    import properties from react router dom, properties for react redux, and some bootstrap
    components
    decontruct match, location, and history in the cart screen func to use to find params id to set to variable, use location to set the variable of qty to the value in params, and history for redirects
    set cart state to store cartItems into variable to be used to show items
    use conditional to find product id to dispatch the addToCart action

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-router-bootstrap'
+import { addToCart } from '../actions/cartActions'
+ 
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id
+ // use location to set the value of the params to the qty variable, qty will be set to the value after the ? in the params, .split is used to get just the number after the equal
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
-const CartScreen = () => {
+  const dispatch = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+
+  const { cartItems } = cart 
+  
+  useEffect(() => {
+    if(productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
   return (
     <div>
       <h2>Cart Screen</h2>
