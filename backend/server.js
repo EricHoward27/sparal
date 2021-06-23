72c03b9755dca4261a4e286312214490861c81bf
eric <erichoward2141@gmail.com>
2021-06-23 14:07:36 -0500

    Sparal 1.6.5
    clean up product routes in backend by using controller and pointed routes to the controller for products
    create user routes, and controllers to manage users and authentication
    test routes in postman to ensure was able to retrieve users
    in user model import bcrypte to compare the passwords due to it being encrptyed in db, compare password and use condition to find user in the controller
    in the user controller find user by email, if user is found and password match return the user data in json format else will send error status and message

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import connectDb from './config/db.js'
 // custom routes
 import productRoutes from './routes/product_routes.js'
+import userRoutes from './routes/user_routes.js'
 // custom middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -12,12 +13,15 @@ dotenv.config()
 connectDb()
 
 const app = express()
+// body parser
+app.use(express.json())
 
 app.get('/', (req, res ) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // error handler for 404
 app.use(notFound)
