f1f1565fa4a2dbf8e89983b90af6b98418af382d
eric <erichoward2141@gmail.com>
2021-06-24 14:20:29 -0500

    Sparal 1.6.7
    import jwt package to be used to generate token
    create new util folder and generate token file that function to generate the token when user sign in
    in .env create a jwt secret
    import file into user controller and set up token in user profile, set payload to user id
    made new file for auth middleware that will verify token and store user id into the req.user, and set up errors if token fail or not found using aync handler
    store user with profile data by searching for id and if found return in json the user data otherwise send error
    test routes in postman and was a success

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../util/generateToken.js'
 // import models
 import User from '../models/user.js'
 
@@ -16,14 +17,33 @@ const authUser = asyncHandler(async (req, res) => {
     name: user.name,
     email: user.email,
     isAdmin: user.isAdmin,
-    token: null
+    token: generateToken(user._id)
   })
  } else {
     res.status(401)
     throw new Error('Invalid email or password')
  }
 })
-
+// @desc Get user profile
+// @route Get /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+// if user is found return the user profile data
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+// throw error if user not found
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 export { 
-  authUser
+  authUser,
+  getUserProfile
 }
